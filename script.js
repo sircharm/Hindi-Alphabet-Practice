5899260fa7890a071272ec625a8c1aaf5d7ade4a
Marcelo Bernardes de Oliveira <marcelo.web13@gmail.com>
2025-11-18 17:30:29 +0000

    Created a basic script to show a random image of a character and a form.

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-const button = document.getElementById("myButton");
-const output = document.getElementById("output");
-button.addEventListener("click", () => {
-  alert("Hello, World");
-});
+// max should be set to the lenght of the effective array
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function arrayShuffle(array) {
+    let curr = array.length - 1;
+    while (curr > 0) {
+        let rand = getRandomInt(array.length);
+        
+        let temp = array[curr];
+        array[curr] = array[rand];
+        array[rand] = temp;
+
+        curr--;
+    }
+    return array;
+}
+
+function answerCheck(answer){
+    //
+}
+
+const images = [
+    // VOWELS
+    [  "a",  "./images/vowels/a.png"], // 0
+    [ "aa", "./images/vowels/aa.png"], // 1
+    [  "i",  "./images/vowels/i.png"], // 2
+    [ "ii", "./images/vowels/ii.png"], // 3
+    [  "u",  "./images/vowels/u.png"], // 4
+    [ "uu", "./images/vowels/uu.png"], // 5
+    [  "e",  "./images/vowels/e.png"], // 6
+    [ "ai", "./images/vowels/ai.png"], // 7
+    [  "o",  "./images/vowels/o.png"], // 8
+    [ "au", "./images/vowels/au.png"] // 9
+];
