283a4e5decbbb09dd5d1fd1b4a3721032da6aacc
Marcelo Bernardes de Oliveira <marcelo.web13@gmail.com>
2025-11-19 18:29:39 +0000

    Added matras

--- script.js
+++ script.js
@@ -36,5 +36,19 @@ const images = [
     [  "e",  "./images/vowels/e.png"], // 6
     [ "ai", "./images/vowels/ai.png"], // 7
     [  "o",  "./images/vowels/o.png"], // 8
-    [ "au", "./images/vowels/au.png"]  // 9
+    [ "au", "./images/vowels/au.png"],  // 9
+    // MATRAS
+    [ "ka",  "./images/Matras/ka.png"], // 10
+    ["kaa", "./images/Matras/kaa.png"], // 11
+    ["kai", "./images/Matras/kai.png"], // 12
+    ["kan", "./images/Matras/kan.png"], // 13
+    ["kau", "./images/Matras/kau.png"], // 14
+    [ "ke",  "./images/Matras/ke.png"], // 15
+    [ "ki",  "./images/Matras/ki.png"], // 16
+    ["kii", "./images/Matras/kii.png"], // 17
+    ["kka", "./images/Matras/kka.png"], // 18
+    [ "ko",  "./images/Matras/ko.png"], // 19
+    [ "ku",  "./images/Matras/ku.png"], // 20
+    ["kuu", "./images/Matras/kuu.png"], // 21
+    // EXTRA CHARACTERS
 ];
