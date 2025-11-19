a608ce8788f521729d18a17e6a1a79b3aaeaecbd
Marcelo Bernardes de Oliveira <marcelo.web13@gmail.com>
2025-11-19 19:05:18 +0000

    Added the remaining characters

--- script.js
+++ script.js
@@ -51,4 +51,44 @@ const images = [
     [ "ku",  "./images/Matras/ku.png"], // 20
     ["kuu", "./images/Matras/kuu.png"], // 21
     // EXTRA CHARACTERS
+    [  "fa",   "./images/Extra/fa.png"], // 22
+    [ "gya",  "./images/Extra/gya.png"], // 23
+    ["ksha", "./images/Extra/ksha.png"], // 24
+    ["shra", "./images/Extra/shra.png"], // 25
+    [ "tra",  "./images/Extra/tra.png"], // 26
+    [  "za",   "./images/Extra/za.png"], // 27
+    // CONSONANTS
+    [  "ba",   "./images/Consonants/ba.png"], // 28
+    [ "bha",  "./images/Consonants/bha.png"], // 29
+    [ "cha",  "./images/Consonants/cha.png"], // 30
+    ["ccha", "./images/Consonants/ccha.png"], // 31
+    [ "dda",  "./images/Consonants/dda.png"], // 32
+    ["ddha", "./images/Consonants/ddha.png"], // 33
+    [ "dha",  "./images/Consonants/dha.png"], // 34
+    [  "ga",   "./images/Consonants/ga.png"], // 35
+    [ "gha",  "./images/Consonants/gha.png"], // 36
+    [  "ha",   "./images/Consonants/ha.png"], // 37
+    [  "ja",   "./images/Consonants/ja.png"], // 38
+    [ "jha",  "./images/Consonants/jha.png"], // 39
+    [ "kha",  "./images/Consonants/kha.png"], // 40
+    [  "la",   "./images/Consonants/la.png"], // 41
+    [  "ma",   "./images/Consonants/ma.png"], // 42
+    [  "na",   "./images/Consonants/na.png"], // 43
+    [ "nga",  "./images/Consonants/nga.png"], // 44
+    [ "nna",  "./images/Consonants/nna.png"], // 45
+    [ "nya",  "./images/Consonants/nya.png"], // 46
+    [  "pa",   "./images/Consonants/pa.png"], // 47
+    [ "pha",  "./images/Consonants/pha.png"], // 48
+    [  "ra",   "./images/Consonants/ra.png"], // 49
+    [ "rra",  "./images/Consonants/rra.png"], // 50
+    ["rrha", "./images/Consonants/rrha.png"], // 51
+    [  "sa",   "./images/Consonants/sa.png"], // 52
+    [ "sha",  "./images/Consonants/sha.png"], // 53
+    [ "ssa",  "./images/Consonants/ssa.png"], // 54
+    [  "ta",   "./images/Consonants/ta.png"], // 55
+    [ "tha",  "./images/Consonants/tha.png"], // 56
+    [ "tta",  "./images/Consonants/tta.png"], // 57
+    ["ttha", "./images/Consonants/ttha.png"], // 58
+    [  "va",   "./images/Consonants/va.png"], // 59
+    [  "ya",   "./images/Consonants/ya.png"], // 60
 ];
