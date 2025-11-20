24827e5d924ebba41d835cc1ae0da528110c3805
Marcelo Bernardes de Oliveira <marcelo.web13@gmail.com>
2025-11-20 14:33:18 +0000

    minor correction in the finalMessage function

--- script.js
+++ script.js
@@ -38,13 +38,12 @@ function finalMessage(rights, all){
         if (all.length < 61) {
             all.unshift(all.length);
         }
-    }
     } else{
         alert(`Oof, you only got ${rights.length} out of ${all.length} questions right!`);
     }
-    
     //shuffles the questions array to restart the game
     all = arrayShuffle(all);
+    }
 
 const images = [
     // VOWELS
